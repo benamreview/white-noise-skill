0b18a886b96fd2762a8fab16faf08c012065eb23
benamreview <dhh3hb@mail.umkc.edu>
2019-04-07 02:06:39 -0500

    commented code out

--- __init__.py
+++ __init__.py
@@ -44,6 +44,7 @@ class WhiteNoise(MycroftSkill):
             self.settings["sounds_dir"] = join(dirname(__file__), "sounds")
         self.p = None
         self.settings.set_changed_callback(self._fix_gender)
+        
 
     def _fix_gender(self):
         if "f" in self.settings["gender"].lower():
@@ -52,6 +53,13 @@ class WhiteNoise(MycroftSkill):
             self.settings["gender"] = "male"
         else:
             self.settings["gender"] = "robot"
+        #Build play list
+        self.play_list = {
+            'ocean': join(abspath(dirname(__file__)), 'sound', 'ocean.wav'),    
+            'wind': join(abspath(dirname(__file__)), 'sound', 'wind.wav'),
+            'train': join(abspath(dirname(__file__)), 'sound', 'rain.wav'),
+        
+        }
 
     def initialize(self):
         sounds_dir = join(self.settings["sounds_dir"], "male")
@@ -69,56 +77,91 @@ class WhiteNoise(MycroftSkill):
                                 ".wav" in sound or ".mp3" in sound]
         # stop laughs for speech execution
         self.add_event("speak", self.stop_laugh)
+        
+    #Play random story from list
+    @intent_file_handler('noise.white.intent')
+    def handle_stories_bedtime(self, message):
+        wait_while_speaking()
+        story_file = list(self.play_list.values())
+        story_file = random.choice(story_file)
+        print(story_file)
+        #if os.path.isfile(story_file):
+        wait_while_speaking()
+        self.process = play_wav(story_file)
 
-    def laugh(self):
-        # dont laugh over a speech message
-        if is_speaking():
-            wait_while_speaking()
-
-        sound = random.choice(self.sounds[self.settings["gender"]])
-        if ".mp3" in sound:
-            self.p = play_mp3(sound)
-        elif ".ogg" in sound:
-            self.p = play_ogg(sound)
+    #Pick story by title
+    @intent_file_handler('noise.white.intent')
+    def handle_pick_story(self, message):
+        wait_while_speaking()
+        title = message.data.get('sound')
+        score = match_one(title, self.play_list)
+        print(score)
+        if score[1] > 0.5:
+            self.process = play_wav(score[0])
         else:
-            self.p = play_wav(sound)
-
-    @intent_file_handler("Laugh.intent")
-    def handle_laugh_intent(self, message):
-        self.laugh()
-
-    @intent_file_handler("RandomLaugh.intent")
-    def handle_random_intent(self, message):
-        # initiate random laughing
-        self.log.info("Laughing skill: Triggering random laughing")
-        self.random_laugh = True
-        self.handle_laugh_event(message)
-
-    @intent_handler(
-        IntentBuilder('StopLaughing').require('Stop').require('Laugh'))
-    def halt_laughing(self, message):
-        self.log.info("Laughing skill: Stopping")
-        # if in random laugh mode, cancel the scheduled event
-        if self.random_laugh:
-            self.log.info("Laughing skill: Stopping random laugh event")
-            self.random_laugh = False
-            self.cancel_scheduled_event('random_laugh')
-            self.speak_dialog("cancel")
-        else:
-            self.speak_dialog("cancel_fail")
-
-    def handle_laugh_event(self, message):
-        # create a scheduled event to laugh at a random interval between 1
-        # minute and half an hour
-        if not self.random_laugh:
-            return
-        self.log.info("Laughing skill: Handling laugh event")
-        self.laugh()
-        self.cancel_scheduled_event('random_laugh')
-        self.schedule_event(self.handle_laugh_event,
-                            datetime.now() + timedelta(
-                                seconds=random.randrange(60, 1800)),
-                            name='random_laugh')
+            return None
+            self.speak('Sorry I could not find that sound in my library')
+##    def laugh(self):
+##        # dont laugh over a speech message
+##        if is_speaking():
+##            wait_while_speaking()
+##        
+##        sound = random.choice(self.sounds[self.settings["gender"]])
+##        if ".mp3" in sound:
+##            self.p = play_mp3(sound)
+##        elif ".ogg" in sound:
+##            self.p = play_ogg(sound)
+##        else:
+##            self.p = play_wav(sound)
+##    def laugh(self):
+##        # dont laugh over a speech message
+##        if is_speaking():
+##            wait_while_speaking()
+##
+##        sound = random.choice(self.sounds[self.settings["gender"]])
+##        if ".mp3" in sound:
+##            self.p = play_mp3(sound)
+##        elif ".ogg" in sound:
+##            self.p = play_ogg(sound)
+##        else:
+##            self.p = play_wav(sound)
+##
+##    @intent_file_handler("Laugh.intent")
+##    def handle_laugh_intent(self, message):
+##        self.laugh()
+##
+##    @intent_file_handler("RandomLaugh.intent")
+##    def handle_random_intent(self, message):
+##        # initiate random laughing
+##        self.log.info("Laughing skill: Triggering random laughing")
+##        self.random_laugh = True
+##        self.handle_laugh_event(message)
+##
+##    @intent_handler(
+##        IntentBuilder('StopLaughing').require('Stop').require('Laugh'))
+##    def halt_laughing(self, message):
+##        self.log.info("Laughing skill: Stopping")
+##        # if in random laugh mode, cancel the scheduled event
+##        if self.random_laugh:
+##            self.log.info("Laughing skill: Stopping random laugh event")
+##            self.random_laugh = False
+##            self.cancel_scheduled_event('random_laugh')
+##            self.speak_dialog("cancel")
+##        else:
+##            self.speak_dialog("cancel_fail")
+##
+##    def handle_laugh_event(self, message):
+##        # create a scheduled event to laugh at a random interval between 1
+##        # minute and half an hour
+##        if not self.random_laugh:
+##            return
+##        self.log.info("Laughing skill: Handling laugh event")
+##        self.laugh()
+##        self.cancel_scheduled_event('random_laugh')
+##        self.schedule_event(self.handle_laugh_event,
+##                            datetime.now() + timedelta(
+##                                seconds=random.randrange(60, 1800)),
+##                            name='random_laugh')
 
     def stop_laugh(self):
         if self.p is not None:
